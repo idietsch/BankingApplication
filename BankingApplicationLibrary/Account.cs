56fb19b728f86d5be31f97e61fa63f09cbc57095
Ian Dietsch <wolffox36007@gmail.com>
2020-02-12 16:24:09 -0500

    Add project files.

---
+++ BankingApplicationLibrary/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankingApplicationLibrary {
+    
+    public class Account {
+        public decimal Balance { get; set; }
+        public string AcctNbr { get; set; }
+        public string Description { get; set; }
+        
+        public void Deposit(decimal amount) {
+
+        }
+        public void Withdraw(decimal amount) {
+
+        }
+        public void Transfer(decimal amount, Account toAccount, Account fromAccount) {
+
+        }
+
+        public Account() { }
+    }
+}
